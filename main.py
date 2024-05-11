61893f4a17d2244571c04187afad9129e916cc10
kelvinleandro <kelvinleandro5@gmail.com>
2024-05-11 10:31:14 -0300

    feat: add table columns dimension

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import curses, socket, threading
+import curses, socket, threading, math
 # import pandas as pd
 
 # key options
@@ -13,7 +13,7 @@ PROTOCOLS_OPTIONS = ["ALL", "TCP", "UDP", "ICMP"]
 
 def display_options(stdscr) -> None:
   # stdscr.clear()
-  h, w = stdscr.getmaxyx()
+  h, w = stdscr.getmaxyx() # screen dimension
   menu_str = ""
   for key, value in OPTIONS.items():
     menu_str += f"{key} {value}  "
@@ -30,7 +30,7 @@ def display_options(stdscr) -> None:
 
   stdscr.refresh()
 
-def display_status(stdscr, proto: int, capture_state: str) -> None:
+def display_status(stdscr, proto: int, capture_state: str, counter: int) -> None:
   # text colors
   curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
   curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -40,10 +40,24 @@ def display_status(stdscr, proto: int, capture_state: str) -> None:
   stdscr.addstr(1, 2, "Capture: ")
   stdscr.addstr(1, 11, f"{capture_state:<3}", curses.color_pair(1 if capture_state == "OFF" else 2))
   stdscr.addstr(1, 17, '\t' + protocol_text)
+  stdscr.addstr(1, 25 + len(protocol_text), '\t' + f"Packets captured: {counter}")
   stdscr.refresh()
 
 def display_table(stdscr) -> None:
-  h, w = stdscr.getmaxyx()
+  # TODO: show columns
+  # TODO: show table
+  h, w = stdscr.getmaxyx() # screen dimension
+  max_cols = w-2 # excluding borders
+  columns = ["Number", "Time", "MAC address src", "MAC address dst", "Protocol", "Length"]
+  cols_dim = {
+    "number": math.floor(max_cols * 0.1),
+    "time": math.floor(max_cols * 0.1),
+    "mac_src": math.floor(max_cols * 0.3),
+    "mac_dst": math.floor(max_cols * 0.3),
+    "protocol": math.floor(max_cols * 0.1),
+    "length": math.floor(max_cols * 0.1),
+  }
+
   stdscr.addstr(h-2, 3, f"h: {h} ")
   stdscr.addstr(h-2, 10, f"w: {w}")
   stdscr.refresh()
@@ -72,12 +86,12 @@ def main(stdscr) -> None:
   sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
   sock.setblocking(False)
   counter = [0]
-  start_stop_state = ["OFF"]
+  enable = ["OFF"]
   transport_filter = 0
 
   stdscr.refresh()
   curses.curs_set(0)  # Hide the cursor
-  stdscr.nodelay(True)
+  stdscr.nodelay(True) # making getch() non-blocking
   MAX_HEIGHT, MAX_WIDTH = stdscr.getmaxyx() # Screen max width and height  
 
   # Create top window
@@ -100,13 +114,9 @@ def main(stdscr) -> None:
   while True:
     # stdscr.clear()
     display_options(win_bottom)
-    display_status(win_top, transport_filter, start_stop_state[0])
+    display_status(win_top, transport_filter, enable[0], counter[0])
 
     display_table(win_mid_l)
-
-    if start_stop_state[0] == "ON":
-      win_mid_l.addstr(2, 5, f"Packets captured: {counter[0]}")
-      win_mid_l.refresh()
 
     try:
       key = stdscr.getch()
@@ -114,13 +124,13 @@ def main(stdscr) -> None:
       key = None
 
     if key == ord('q'):
-      break  # Exit the loop if 'Q' is pressed
+      break
     elif key == ord('s'):
-      if start_stop_state[0] == "ON":
-        start_stop_state[0] = "OFF"
+      if enable[0] == "ON":
+        enable[0] = "OFF"
       else:
-        start_stop_state[0] = "ON"
-        threading.Thread(target=start_packet_capture, args=(start_stop_state, sock, counter)).start()
+        enable[0] = "ON"
+        threading.Thread(target=start_packet_capture, args=(enable, sock, counter)).start()
     elif key == ord('f'):
       # Cycle through the elements
       transport_filter = (transport_filter + 1) % len(PROTOCOLS_OPTIONS)
